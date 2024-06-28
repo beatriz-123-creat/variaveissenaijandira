78220caf04f69c692c4575ee3960d6a06f4a3a3b
celso.alunos <joaopedro.dancam@gmail.com>
2024-06-28 14:52:49 -0300

    Finalizado

---
+++ src/Variaveis.java
@@ -0,0 +1,24 @@
+public class Variaveis {
+
+    public static void main(String[] args) {
+        //programa de um funcionamento de pet shop
+       byte idadeDoPet = 9;
+
+       short valorDaConsulta = 100;
+
+       double salarioDoFuncionarioResponsavelPeloBanho = 6373.78;
+
+       String nomeDoPet = "Thomas";
+       String medicoDoPet = "Ethan";
+
+        //saída dos dados para o cliente da pet shop
+       System.out.println("                    Saida do programa                     ");
+       System.out.println("----------------------------------------------------------");
+       System.out.println("Salário do funcionario responsavel pelo banho: " + salarioDoFuncionarioResponsavelPeloBanho);
+       System.out.println("Idade do Pet: " + idadeDoPet);
+       System.out.println("Valor da Consulta: " + valorDaConsulta);
+       System.out.println("O " + nomeDoPet + " foi atendido pelo " + medicoDoPet + ".");
+       System.out.println("----------------------------------------------------------");
+    }
+
+}
